4e6826fdd5c9f8d3ac348b03509eb405faf261a7
Chris Hartley <chris.hartley@renewindianapolis.org>
2024-12-11 10:14:24 -0500

    Continud tweaks to work in production

--- config/backend/start-local.sh
+++ config/backend/start-local.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-python /code/backend/manage.py migrate
-python /code/backend/manage.py runserver 0.0.0.0:8000
+python /code/planning_notifier/manage.py migrate
+python /code/planning_notifier/manage.py runserver 0.0.0.0:8000
--- config/backend/start.sh
+++ config/backend/start.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-python /code/backend/manage.py migrate
-cd /code/backend
+python /code/planning_notifier/manage.py migrate
+cd /code/planning_notifier
 gunicorn --bind=0.0.0.0 backend.wsgi
