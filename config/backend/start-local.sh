062341fa6a0abace958adf01bb769a8b0caef211
Chris Hartley <chris.hartley@renewindianapolis.org>
2024-12-10 20:40:00 -0500

    More complex config, copied from CPT

--- config/backend/start-local.sh
+++ config/backend/start-local.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-python /code/manage.py migrate
-python /code/manage.py runserver 0.0.0.0:8000
+python /code/backend/manage.py migrate
+python /code/backend/manage.py runserver 0.0.0.0:8000
 #cd /code/backend
 #gunicorn --bind=0.0.0.0 backend.wsgi
