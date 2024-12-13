5e1959b7f42f6afd76f8d99945c70eff694b01ed
Chris Hartley <chris.hartley@renewindianapolis.org>
2024-12-13 14:54:21 -0500

    Use project level templates

--- planning_notifier/planning_notifier/settings.py
+++ planning_notifier/planning_notifier/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'planning_notifier.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
