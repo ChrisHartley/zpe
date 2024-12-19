138fcfbeecf639c8c2c847169422baa0db327950
Chris Hartley <chris.hartley@renewindianapolis.org>
2024-12-18 20:04:47 -0500

    maybe not progress

--- planning_notifier/planning_cases/admin.py
+++ planning_notifier/planning_cases/admin.py
@@ -7,8 +7,9 @@ class planning_caseAdmin(admin.GISModelAdmin):
     list_display = ['case_date', 'case_number', 'parcel_number', 'description']
 
 class area_of_interestAdmin(admin.GISModelAdmin):
-    raw_id_fields = ["planning_cases"]
+#    raw_id_fields = ["planning_cases"]
 #    readonly_fields = ["planning_cases"]
+    pass
 
 admin.site.register(planning_case, planning_caseAdmin)
 admin.site.register(area_of_interest, area_of_interestAdmin)
