943e52b27cadf7c50e81ca70d639431a3b3e87c3
Chris Hartley <chris.hartley@renewindianapolis.org>
2025-01-11 13:28:42 -0500

    migrate

--- config/backend/start-local.sh
+++ config/backend/start-local.sh
@@ -4,5 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+python /code/planning_notifier/manage.py makemigrations
 python /code/planning_notifier/manage.py migrate
 python /code/planning_notifier/manage.py runserver 0.0.0.0:8000
